267ef89c777055cf51426d52ba85915dd34380bf
Brandon <hewitt205@yahoo.com>
2025-07-17 21:12:02 -0500

    continuing on

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,14 +3,17 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+	"github.com/hewitt205/pokedexGo/internal/pokecache"
 )
 
 type Client struct {
+	cache pokecache.Cache
 	httpClient http.Client
 }
 
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 
 func main() {
-	pokeClient := pokeapi.NewClient(5 * time.Second)
+	pokeClient := pokeapi.NewClient(5 * time.Second, time.Minute*5)
 	config := &config{
 		pokeapiClient: pokeClient,
 	}
